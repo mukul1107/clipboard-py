4e3972038addff4ef01f9961686ff22f244d158e
mukul1107 <mukulsingh3078@gmail.com>
2024-03-14 11:12:10 +0530

    .

--- mClip.py
+++ mClip.py
@@ -7,6 +7,7 @@ TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
         'busy': """Sorry, can we do this later this week or next week?""",
         'upsell': """Would you consider making this a monthly donation?"""}
 
+
 if len(sys.argv) < 2:
     print('Usage: python mClip.py [keyphrase] - copy phrase text')
     sys.exit()
